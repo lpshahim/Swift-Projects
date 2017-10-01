862b14e5339ba7909f1d8d418dc6a9f56fc57ef4
lpshahim <lp.shahim6@gmail.com>
2017-10-01 21:07:13 +0200

    Security application added

---
+++ Cipher Security Application/SecurishApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecurishApp
+//
+//  Created by Louis-Philip Shahim on 2016/03/10.
+//  Copyright © 2016 Louis-Philip Shahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
